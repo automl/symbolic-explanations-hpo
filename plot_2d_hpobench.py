bb9846ddb1f756020f9a8bbe4700261dade6921d
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-03-22 11:28:21 +0100

    Plot 2d hpobench

---
+++ plot_2d_hpobench.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import os
+import numpy as np
+import dill as pickle
+
+from utils.run_utils import plot_symb2d, get_surrogate_predictions, convert_symb
+from utils.logging_utils import get_logger
+
+from utils.hpobench_utils import get_run_config, get_benchmark_dict, get_task_dict
+
+
+if __name__ == "__main__":
+    symb_dir_name = f"parsimony0.0001"
+    dir_with_test_data = "learning_curves/runs_surr_hpobench"
+    n_optimized_params = 2
+    # if None, average metrics over all sample sizes
+    n_samples = 100
+    max_hp_comb = 1
+
+    n_samples_spacing = np.linspace(20, 200, 10, dtype=int).tolist()
+    n_test_samples = 100
+    symb_seeds = [0] #, 3, 6]
+    evaluate_on_surrogate = True
+
+    init_design_max_ratio = 0.25
+    init_design_n_configs_per_hyperparamter = 8
+
+    run_configs = get_run_config(n_optimized_params=n_optimized_params, max_hp_comb=max_hp_comb)
+
+    # set up directories
+    plot_dir = f"learning_curves/plots"
+    viz_plot_dir = f"{plot_dir}/visualization"
+    if not os.path.exists(viz_plot_dir):
+        os.makedirs(viz_plot_dir)
+
+    logger = get_logger(filename=f"{plot_dir}/plot_log.log")
+
+    logger.info(f"Save plots to {plot_dir}.")
+
+    for run_conf in run_configs:
+        task_dict = get_task_dict()
+        data_set = f"{task_dict[run_conf['task_id']]}"
+        optimized_parameters = list(run_conf["hp_conf"])
+        model_name = get_benchmark_dict()[run_conf["benchmark"]]
+        b = run_conf["benchmark"](task_id=run_conf["task_id"], hyperparameters=optimized_parameters)
+
+        # add only parameters to be optimized to configspace
+        cs = b.get_configuration_space(hyperparameters=optimized_parameters)
+
+        run_name = f"{model_name.replace(' ', '_')}_{'_'.join(optimized_parameters)}_{data_set}"
+        logger.info(f"Create plot for {run_name}.")
+
+        sampling_dir_smac = f"learning_curves/runs_sampling_hpobench/smac/{run_name}"
+        sampling_dir_rand = f"learning_curves/runs_sampling_hpobench/rand/{run_name}"
+
+        symb_dir_surr = f"learning_curves/runs_symb_hpobench/{symb_dir_name}/surr/{run_name}/symb_models"
+        symb_dir_smac = f"learning_curves/runs_symb_hpobench/{symb_dir_name}/smac/{run_name}/symb_models"
+        symb_dir_rand = f"learning_curves/runs_symb_hpobench/{symb_dir_name}/rand/{run_name}/symb_models"
+
+        # Get specific surrogate file for each sample size for which the number of initial designs differs from
+        # the maximum number of initial designs (number of hyperparameters * init_design_n_configs_per_hyperparamter)
+        if init_design_max_ratio * n_samples < len(
+                optimized_parameters) * init_design_n_configs_per_hyperparamter:
+            n_eval = n_samples
+        else:
+            n_eval = max(n_samples_spacing)
+        df_samples_smac = pd.read_csv(f"{sampling_dir_smac}/samples_{n_eval}.csv")
+        df_samples_rand = pd.read_csv(f"{sampling_dir_rand}/samples_{max(n_samples_spacing)}.csv")
+
+        # Load test data
+        logger.info(f"Get test data for {run_name}.")
+        X_test = np.array(pd.read_csv(f"{dir_with_test_data}/{run_name}/x_test.csv"))
+        y_test = np.array(pd.read_csv(f"{dir_with_test_data}/{run_name}/y_test.csv"))
+
+        for sampling_seed in [0]: #df_samples_smac.seed.unique():
+            logger.info(f"Considering sampling seed {sampling_seed}.")
+            df_sampling_seed_smac = df_samples_smac.copy()[df_samples_smac["seed"] == sampling_seed]
+            df_sampling_seed_rand = df_samples_rand.copy()[df_samples_rand["seed"] == sampling_seed]
+
+            X_train_smac = np.array(df_sampling_seed_smac[[optimized_parameters[0], optimized_parameters[1]]])[:n_samples]
+            X_train_rand = np.array(df_sampling_seed_rand[[optimized_parameters[0], optimized_parameters[1]]])[:n_samples]
+
+            for symb_seed in symb_seeds:
+                logger.info(f"Considering symb seed {symb_seed}.")
+
+                predictions_test = {}
+
+                if evaluate_on_surrogate:
+                    with open(
+                            f"{symb_dir_surr}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                            "rb") as symb_model_file_surr:
+                        symb_surr = pickle.load(symb_model_file_surr)
+                    symb_pred_surr = symb_surr.predict(
+                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                        ).reshape(X_test.shape[2], X_test.shape[1]).T
+                    surr_conv = convert_symb(symb_surr, n_decimals=3)
+                    if len(str(surr_conv)) < 80:
+                        predictions_test[f"SR (BO-GP): {surr_conv}"] = symb_pred_surr
+                    else:
+                        predictions_test[f"SR (BO-GP)"] = symb_pred_surr
+
+                    surr_dir = f"learning_curves/runs_surr/{run_name}"
+                    with open(
+                            f"{sampling_dir_smac}/surrogates/n_eval{n_eval}_samples{n_samples}_seed{sampling_seed}.pkl",
+                            "rb") as surrogate_file:
+                        surrogate_model = pickle.load(surrogate_file)
+                    predictions_test["GP (BO)"] = np.array(get_surrogate_predictions(
+                        X_test.reshape(len(optimized_parameters), -1).T, cs, surrogate_model)).reshape(
+                        X_test.shape[1], X_test.shape[2])
+                    X_train_list = [X_train_smac.T, None]
+                else:
+                    with open(
+                            f"{symb_dir_smac}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                            "rb") as symb_model_file_smac:
+                        symb_smac = pickle.load(symb_model_file_smac)
+                    symb_pred_smac = symb_smac.predict(
+                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                        ).reshape(X_test.shape[2], X_test.shape[1]).T
+                    smac_conv = convert_symb(symb_smac, n_decimals=3)
+                    if len(str(smac_conv)) < 80:
+                        predictions_test[f"SR (BO): {smac_conv}"] = symb_pred_smac
+                    else:
+                        predictions_test[f"SR (BO)"] = symb_pred_smac
+
+                    with open(
+                            f"{symb_dir_rand}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                            "rb") as symb_model_file_rand:
+                        symb_rand = pickle.load(symb_model_file_rand)
+                    symb_prad_rand = symb_rand.predict(
+                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                        ).reshape(X_test.shape[2], X_test.shape[1]).T
+                    rand_conv = convert_symb(symb_rand, n_decimals=3)
+                    if len(str(rand_conv)) < 80:
+                        predictions_test[f"SR (Random): {rand_conv}"] = symb_prad_rand
+                    else:
+                        predictions_test[f"SR (Random)"] = symb_prad_rand
+
+                    X_train_list = [X_train_smac.T, X_train_rand.T]
+
+                filename = f"{run_name}_n_samples{n_samples}_" \
+                           f"sampling_seed{sampling_seed}_symb_seed{symb_seed}"
+                if evaluate_on_surrogate:
+                    filename = "_".join([filename, "surrogate"])
+
+                plot = plot_symb2d(
+                                X_train_list=X_train_list,
+                                X_test=X_test,
+                                y_test=y_test,
+                                function_name=model_name,
+                                metric_name=r'$\mathcal{L}$_{Test}',
+                                predictions_test=predictions_test,
+                                parameters=optimized_parameters,
+                                plot_dir=viz_plot_dir,
+                                filename=filename
+                            )
