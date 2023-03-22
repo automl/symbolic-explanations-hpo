cd42c93bb82a383c67d46bf690613d2ddf852d1d
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-03-22 07:50:31 +0100

    Parsimony

--- run_symbolic_explanation_hpobench.py
+++ run_symbolic_explanation_hpobench.py
@@ -23,19 +23,19 @@ if __name__ == "__main__":
     evaluate_on_surrogate = True
 
     max_hp_comb = 1
-    #parsimony_coefficient_space = [0.0005]
-    parsimony_coefficient_space = [
-        0.000001, 0.0000025, 0.000005, 0.0000075,
-        0.00001, 0.000025, 0.00005, 0.000075,
-        0.0001, 0.00025, 0.00075, #0.0005
-        0.001, 0.0025, 0.005, 0.0075,
-        0.01, 0.025, 0.05, 0.075
-    ]
+    parsimony_coefficient_space = [0.0005]
+    # parsimony_coefficient_space = [
+    #     0.000001, 0.0000025, 0.000005, 0.0000075,
+    #     0.00001, 0.000025, 0.00005, 0.000075,
+    #     0.0001, 0.00025, 0.00075, #0.0005
+    #     0.001, 0.0025, 0.005, 0.0075,
+    #     0.01, 0.025, 0.05, 0.075
+    # ]
 
     sampling_dir_name = "runs_sampling_hpobench"
     dir_with_test_data = "learning_curves/runs_surr_hpobench"
     n_optimized_params = 2
-    n_samples_spacing = [200] # np.linspace(20, 200, 10, dtype=int).tolist()
+    n_samples_spacing = np.linspace(20, 200, 10, dtype=int).tolist()
     n_seeds = 3
     n_test_samples = 100
     init_design_max_ratio = 0.25
