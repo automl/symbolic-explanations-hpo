4a94145801fa6b608a3b5421a762fa7ff948848a
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-03-23 00:16:36 +0100

    Plot

--- plot_2d_hpobench.py
+++ plot_2d_hpobench.py
@@ -90,72 +90,77 @@ if __name__ == "__main__":
                 logger.info(f"Considering symb seed {symb_seed}.")
 
                 predictions_test = {}
-
-                if evaluate_on_surrogate:
-                    with open(
-                            f"{symb_dir_surr}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
-                            "rb") as symb_model_file_surr:
-                        symb_surr = pickle.load(symb_model_file_surr)
-                    symb_pred_surr = symb_surr.predict(
-                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
-                        ).reshape(X_test.shape[2], X_test.shape[1]).T
-                    # surr_conv = convert_symb(symb_surr, n_decimals=3)
-                    # if len(str(surr_conv)) < 80:
-                    #     predictions_test[f"SR (BO-GP): {surr_conv}"] = symb_pred_surr
-                    # else:
-                    predictions_test[f"SR (BO-GP)"] = symb_pred_surr
-
-                    surr_dir = f"learning_curves/runs_surr/{run_name}"
-                    with open(
-                            f"{sampling_dir_smac}/surrogates/n_eval{n_eval}_samples{n_samples}_seed{sampling_seed}.pkl",
-                            "rb") as surrogate_file:
-                        surrogate_model = pickle.load(surrogate_file)
-                    predictions_test["GP Baseline"] = np.array(get_surrogate_predictions(
-                        X_test.reshape(len(optimized_parameters), -1).T, cs, surrogate_model)).reshape(
-                        X_test.shape[1], X_test.shape[2])
-                    X_train_list = [None, None]
-                else:
-                    with open(
-                            f"{symb_dir_smac}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
-                            "rb") as symb_model_file_smac:
-                        symb_smac = pickle.load(symb_model_file_smac)
-                    symb_pred_smac = symb_smac.predict(
-                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
-                        ).reshape(X_test.shape[2], X_test.shape[1]).T
-                    # smac_conv = convert_symb(symb_smac, n_decimals=3)
-                    # if len(str(smac_conv)) < 80:
-                    #     predictions_test[f"SR (BO): {smac_conv}"] = symb_pred_smac
-                    # else:
-                    predictions_test[f"SR (BO)"] = symb_pred_smac
-
-                    with open(
-                            f"{symb_dir_rand}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
-                            "rb") as symb_model_file_rand:
-                        symb_rand = pickle.load(symb_model_file_rand)
-                    symb_prad_rand = symb_rand.predict(
-                            X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
-                        ).reshape(X_test.shape[2], X_test.shape[1]).T
-                    rand_conv = convert_symb(symb_rand, n_decimals=3)
-                    if len(str(rand_conv)) < 80:
-                        predictions_test[f"SR (Random): {rand_conv}"] = symb_prad_rand
+                
+                try:
+    
+                    if evaluate_on_surrogate:
+                        with open(
+                                f"{symb_dir_surr}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                                "rb") as symb_model_file_surr:
+                            symb_surr = pickle.load(symb_model_file_surr)
+                        symb_pred_surr = symb_surr.predict(
+                                X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                            ).reshape(X_test.shape[2], X_test.shape[1]).T
+                        # surr_conv = convert_symb(symb_surr, n_decimals=3)
+                        # if len(str(surr_conv)) < 80:
+                        #     predictions_test[f"SR (BO-GP): {surr_conv}"] = symb_pred_surr
+                        # else:
+                        predictions_test[f"SR (BO-GP)"] = symb_pred_surr
+    
+                        surr_dir = f"learning_curves/runs_surr/{run_name}"
+                        with open(
+                                f"{sampling_dir_smac}/surrogates/n_eval{n_eval}_samples{n_samples}_seed{sampling_seed}.pkl",
+                                "rb") as surrogate_file:
+                            surrogate_model = pickle.load(surrogate_file)
+                        predictions_test["GP Baseline"] = np.array(get_surrogate_predictions(
+                            X_test.reshape(len(optimized_parameters), -1).T, cs, surrogate_model)).reshape(
+                            X_test.shape[1], X_test.shape[2])
+                        X_train_list = [None, None]
                     else:
-                        predictions_test[f"SR (Random)"] = symb_prad_rand
+                        with open(
+                                f"{symb_dir_smac}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                                "rb") as symb_model_file_smac:
+                            symb_smac = pickle.load(symb_model_file_smac)
+                        symb_pred_smac = symb_smac.predict(
+                                X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                            ).reshape(X_test.shape[2], X_test.shape[1]).T
+                        # smac_conv = convert_symb(symb_smac, n_decimals=3)
+                        # if len(str(smac_conv)) < 80:
+                        #     predictions_test[f"SR (BO): {smac_conv}"] = symb_pred_smac
+                        # else:
+                        predictions_test[f"SR (BO)"] = symb_pred_smac
+    
+                        with open(
+                                f"{symb_dir_rand}/n_samples{n_samples}_sampling_seed{sampling_seed}_symb_seed{symb_seed}.pkl",
+                                "rb") as symb_model_file_rand:
+                            symb_rand = pickle.load(symb_model_file_rand)
+                        symb_prad_rand = symb_rand.predict(
+                                X_test.T.reshape(X_test.shape[1] * X_test.shape[2], X_test.shape[0])
+                            ).reshape(X_test.shape[2], X_test.shape[1]).T
+                        rand_conv = convert_symb(symb_rand, n_decimals=3)
+                        if len(str(rand_conv)) < 80:
+                            predictions_test[f"SR (Random): {rand_conv}"] = symb_prad_rand
+                        else:
+                            predictions_test[f"SR (Random)"] = symb_prad_rand
+    
+                        X_train_list = [X_train_smac.T, X_train_rand.T]
+    
+                    filename = f"{run_name}_n_samples{n_samples}_" \
+                               f"sampling_seed{sampling_seed}_symb_seed{symb_seed}"
+                    if evaluate_on_surrogate:
+                        filename = "_".join([filename, "surrogate"])
+    
+                    plot = plot_symb2d(
+                                    X_train_list=X_train_list,
+                                    X_test=X_test,
+                                    y_test=y_test,
+                                    function_name=model_name,
+                                    metric_name=r'Validation Loss',
+                                    predictions_test=predictions_test,
+                                    parameters=cs.get_hyperparameters(),
+                                    plot_dir=viz_plot_dir,
+                                    filename=filename
+                                )
 
-                    X_train_list = [X_train_smac.T, X_train_rand.T]
-
-                filename = f"{run_name}_n_samples{n_samples}_" \
-                           f"sampling_seed{sampling_seed}_symb_seed{symb_seed}"
-                if evaluate_on_surrogate:
-                    filename = "_".join([filename, "surrogate"])
-
-                plot = plot_symb2d(
-                                X_train_list=X_train_list,
-                                X_test=X_test,
-                                y_test=y_test,
-                                function_name=model_name,
-                                metric_name=r'Validation Loss',
-                                predictions_test=predictions_test,
-                                parameters=cs.get_hyperparameters(),
-                                plot_dir=viz_plot_dir,
-                                filename=filename
-                            )
+                except:
+                    logger.info(f"Could not process.")
