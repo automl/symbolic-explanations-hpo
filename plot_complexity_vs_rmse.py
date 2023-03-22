94ac0dbc568d84f423f1e0f91fa9cb37e9316f36
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-03-22 10:40:05 +0100

    Plot

--- plot_complexity_vs_rmse.py
+++ plot_complexity_vs_rmse.py
@@ -77,10 +77,8 @@ if __name__ == "__main__":
             df_joined.insert(0, "Parsimony", parsimony)
             df_joined_all = pd.concat((df_joined_all, df_joined))
 
-        logger.info(f"{df_joined_all}")
         df_joined_all['Parsimony'] = df_joined_all['Parsimony'].astype(str)
-
-        df_joined_all.to_csv(f"{plot_dir}/df_joined_all")
+        df_joined_all.to_csv(f"{plot_dir}/df_joined_all_{run_name}")
 
         sns.scatterplot(data=df_joined_all, x="complexity", y="rmse_test", hue="Parsimony",
                         linestyles="")
