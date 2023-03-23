0d34dcc50639eb75c350c350eba38d97482d12eb
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-03-24 00:33:32 +0100

    plot

--- plot_learning_curves_hpobench_subplots_appendix.py
+++ plot_learning_curves_hpobench_subplots_appendix.py
@@ -102,8 +102,8 @@ if __name__ == "__main__":
                 classifier_title = "Neural Network"
             else:
                 classifier_title = model_name
-            param0 = f"log({optimized_parameters[0]})" if cs.get_hyperparameters()[0].log else optimized_parameters[0]
-            param1 = f"log({optimized_parameters[1]})" if cs.get_hyperparameters()[1].log else optimized_parameters[1]
+            param0 = optimized_parameters[0]
+            param1 = optimized_parameters[1]
 
             ind = i + 1
 
