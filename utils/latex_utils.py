4dd7c01e9f200dbb72808103f34a42458fa033cc
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-04-28 14:32:43 +0200

    Linear regression baseline

--- utils/latex_utils.py
+++ utils/latex_utils.py
@@ -30,7 +30,7 @@ def generate_result_table(df, stddev_df, stddev: bool = False, decimal_places: i
     if show_avg:
         df_avg = df.mean()
 
-    df = df.drop(columns=[["GP Baseline", "LR (GP-BO) Baseline"]])
+    df = df.drop(columns=["GP Baseline", "LR (GP-BO) Baseline"])
 
     for k in range(len(df.index)):
         df.iloc[k] = df.iloc[k].apply(
