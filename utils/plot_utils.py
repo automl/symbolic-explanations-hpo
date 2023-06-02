d22894911bf512a9a3da4553d02070151545e84d
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-06-02 14:30:38 +0200

    Format colormesh

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -115,8 +115,8 @@ def plot_symb2d_subplots(
 
         from scipy.interpolate import interp2d
         f = interp2d(X0_test, X1_test, pred_test[ind], kind='cubic')
-        xnew = np.arange(X0_lower, X0_upper, .1)
-        ynew = np.arange(X1_lower, X1_upper, .1)
+        xnew = np.arange(X0_lower, X0_upper, .01)
+        ynew = np.arange(X1_lower, X1_upper, .01)
         data1 = f(xnew, ynew)
         Xn, Yn = np.meshgrid(xnew, ynew)
 
