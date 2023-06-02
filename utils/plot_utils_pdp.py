c310ad28388ee1a2adafeebd98d7b12cd415024b
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-06-02 15:12:47 +0200

    Format colormesh

--- utils/plot_utils_pdp.py
+++ utils/plot_utils_pdp.py
@@ -114,17 +114,17 @@ def plot_symb2d_subplots(
             X1_test,
             pred_test[ind],
             cmap="summer",
-            shading="auto",
+            shading="gouraud",
             vmin=vmin,
             vmax=vmax,
         )
         ax.set_title(f"Prediction: {label}", fontsize=TITLE_SIZE)
         ax.set_xlabel(X0_name, fontsize=TITLE_SIZE)
-        ax.set_xticks(dim_x)
+        #ax.set_xticks(dim_x)
         ax.set_yticks(dim_y)
-        ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
+        #ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
         ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
-        ax.set_xlim(X0_lower, X0_upper)
+        ax.set_xlim(X0_test.min(), X0_test.max())
         ax.set_ylim(X1_lower, X1_upper)
         ax.tick_params(axis="both", which="major", labelsize=LABEL_SIZE)
         ax.grid(alpha=0)
